f8817bc0f39f260966b7ea28edfd9d849f554780
WilmerP <wilmer.wmpac@gmail.com>
2022-09-06 09:37:53 +0200

    LoginVersion1

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import './login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className='Allogin'>
+        <div className='formbg'>
+          <div className='formbg-inner padding-horizontal--48'>
+            <span className='padding-bottom--15'>Accede a tu cuenta de Travel&Working</span>
+            <form id='stripe-login'>
+              <div className='field padding-bottom--24'>
+                <label htmlFor='email'>Email</label>
+                <input type='email' name='email' placeholder='Escriba su correo electrónico' />
+              </div>
+              <div className='field padding-bottom--24'>
+                <div className='grid--50-50'>
+                  <label htmlFor='password'>Contraseña</label>
+                  <div className='reset-pass'></div>
+                </div>
+                <input type='password' name='password' placeholder='Escriba su contraseña' />
+              </div>
+
+              <div className='field padding-bottom--24'>
+                <input type='submit' name='submit' value='Entrar' />
+              </div>
+              <a href='#'>¿Has olvidado tu contraseña?</a>
+              <br />
+              <a href='#'>¿Aún no tienes cuenta?</a>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
