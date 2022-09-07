fa44819bc536a2827bef597fe83b714e336aebeb
chema_Sola10 <chemitata10@gmail.com>
2022-09-07 11:08:52 +0200

    Agregar Footer

--- Client/src/pages/Footer.jsx
+++ Client/src/pages/Footer.jsx
@@ -1,10 +1,31 @@
+import { Link } from 'react-router-dom'
+
 import './footer.css'
 
-
 const Footer = () => (
-  <div className="footer">
-    <p>Contenido de Footer</p>
+  <div className='footer'>
+    <div className='footer__center'>
+      <div className='footer__box'>
+        <div className='footer__title'>TravelWorking</div>
+        <div className='footer__content'>
+          <Link to='/'><p>Principal</p></Link>
+          <Link to='/ofertas'><p>Ofertas</p></Link>
+          <Link to='/login'><p>Login</p></Link>
+          <Link to='/register'><p>Registro</p></Link>
+          
+        </div>
+      </div>
+      <div className='footer__box'>
+        <div className='footer__title'>¿Quienes Somos?</div>
+        <div className='footer__content'>
+          <p>Ismael Cobo</p>
+          <p>Jonotahn</p>
+          <p>Wilmer</p>
+          <p>Chema Solà</p>
+        </div>
+      </div>
+    </div>
   </div>
-);
+)
 
-export default Footer;
+export default Footer
