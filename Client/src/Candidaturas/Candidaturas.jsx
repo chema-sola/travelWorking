8de90c0f6d43f4ac345465bee092a5fc24758a92
Ismael Cobo <midismael@gmail.com>
2022-09-10 10:34:31 +0200

    feat: candidaturas component

--- Client/src/Candidaturas/Candidaturas.jsx
+++ Client/src/Candidaturas/Candidaturas.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+import { useAuthStore } from '../hooks/useAuthStore'
+import { Candidatura } from './components/Candidatura'
+import { Container } from 'react-bootstrap'
+
+import './candidaturas.css'
 
 export const Candidaturas = () => {
-  return <div>Candidaturas</div>
+  const { user } = useAuthStore()
+  const { candidaturas } = user
+  return (
+    <Container style={{ marginTop: '3rem', marginBottom: '3rem' }}>
+      <div className='candidaturas'>
+        {candidaturas.map((candidatura) => (
+          <Candidatura key={candidatura.id} candidatura={candidatura} />
+        ))}
+      </div>
+    </Container>
+  )
 }
