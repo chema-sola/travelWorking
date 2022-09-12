760563b4b431a85011c1cd31e0cc9e4a8ae7323e
chema_Sola10 <chemitata10@gmail.com>
2022-09-12 10:10:15 +0200

    fix PerfilOtraPersona

--- Client/src/Perfil/MiPerfil/MiPerfil.jsx
+++ Client/src/Perfil/MiPerfil/MiPerfil.jsx
@@ -1,3 +1,103 @@
+import './MiPerfil.css'
+import { Link } from 'react-router-dom'
+
+
 export const MiPerfil = ({ user }) => {
-  return <div>{user?.id ? <pre>{JSON.stringify(user, null, 3)}</pre> : <p>Loading...</p>}</div>
+  return (
+    <>
+      <div className='container'>
+        <div className='box'>
+          <p className='h-dash'>Tu Perfil</p>
+          <p className='fst-italic textmuted'>Tu información esta en publico</p>
+          <div className='row mx-0 mt-2'>
+            <div className='col-md-4 p-0 border-end'>
+              <div className='viewbox'>
+                <p>Imagen </p>
+              </div>
+            </div>
+
+            <div className='col-md-8 p-0'>
+              <div className='viewbox'>
+                <p className=''>Descripción</p>
+                <p className='textmuted mb-2'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam dignissimos tenetur similique
+                  sit quas illum. Veniam magnam quisquam ipsam a totam nam. Molestiae praesentium molestias mollitia
+                  voluptatibus autem sint.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className='box2 mt-3'>
+              <div className='d-flex mt-2'>
+                <span className='fas fa-bookmark ms-0 mt-1'></span>
+                <div className='w-100 ps-2 '>
+                  <p className=''>DNI</p>
+                  <p className='textmuted mb-2'>{user.dni}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Nombre</p>
+                  <p className='textmuted mb-2'>{user.nombre}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Apellidos</p>
+                  <p className='textmuted mb-2'>{user.apellidos}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='box2 mt-3'>
+              <div className='d-flex mt-2'>
+                <span className='fas fa-bookmark ms-0 mt-1'></span>
+                <div className='w-100 ps-2 '>
+                  <p className=''>Email</p>
+                  <p className='textmuted mb-2'>{user.email}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Edad </p>
+                  <p className='textmuted mb-2'>{user.edad}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Teléfono</p>
+                  <p className='textmuted mb-2'>{user.telefono}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='box2 mt-3'>
+              <div className='d-flex mt-2'>
+                <span className='fas fa-bookmark ms-0 mt-1'></span>
+                <div className='w-100 ps-2 '>
+                  <p className=''>Pais</p>
+                  <p className='textmuted mb-2'>{user.pais}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Ciudad </p>
+                  <p className='textmuted mb-2'>{user.ciudad}</p>
+                </div>
+                <div className='w-100 ps-2'>
+                  <p className=''>Codigo Postal</p>
+                  <p className='textmuted mb-2'>{user.cpostal}</p>
+                </div>
+              </div>
+            </div>
+
+            
+          </div>
+          <div className='box__title'>
+          <Link to='/ofertas' className='button_mostrarMas'>
+            Editar
+          </Link>
+        </div>
+        </div>
+      </div>
+    </>
+  )
 }
+
+
+/* 
+
+        <div>{user?.id ? <pre>{JSON.stringify(user, null, 3)}</pre> : <p>Loading...</p>}</div>
+
+*/
--- Client/src/Perfil/PerfilOtraPersona/PerfilOtraPersona.jsx
+++ Client/src/Perfil/PerfilOtraPersona/PerfilOtraPersona.jsx
@@ -8,5 +8,92 @@ export const PerfilOtraPersona = ({ id }) => {
     startGettingInfoProfile(id)
   }, [id])
 
-  return <div>{activeUser?.id ? <pre>{JSON.stringify(activeUser, null, 3)}</pre> : <p>Loading...</p>}</div>
+  return (
+    <>
+      {activeUser?.id ? (
+        <div className='container'>
+          <div className='box'>
+            <p className='h-dash'>Tu Perfil</p>
+            <p className='fst-italic textmuted'>Tu información esta en publico</p>
+            <div className='row mx-0 mt-2'>
+              <div className='col-md-4 p-0 border-end'>
+                <div className='viewbox'>
+                  <p>Imagen </p>
+                </div>
+              </div>
+
+              <div className='col-md-8 p-0'>
+                <div className='viewbox'>
+                  <p className=''>Descripción</p>
+                  <p className='textmuted mb-2'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magnam dignissimos tenetur similique
+                    sit quas illum. Veniam magnam quisquam ipsam a totam nam. Molestiae praesentium molestias mollitia
+                    voluptatibus autem sint.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className='box2 mt-3'>
+                <div className='d-flex mt-2'>
+                  <span className='fas fa-bookmark ms-0 mt-1'></span>
+                  <div className='w-100 ps-2 '>
+                    <p className=''>DNI</p>
+                    <p className='textmuted mb-2'>{activeUser.dni}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>Nombre</p>
+                    <p className='textmuted mb-2'>{activeUser.nombre}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>ApellactiveUseros</p>
+                    <p className='textmuted mb-2'>{activeUser.apellidos}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='box2 mt-3'>
+                <div className='d-flex mt-2'>
+                  <span className='fas fa-bookmark ms-0 mt-1'></span>
+                  <div className='w-100 ps-2 '>
+                    <p className=''>Email</p>
+                    <p className='textmuted mb-2'>{activeUser.email}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>Edad </p>
+                    <p className='textmuted mb-2'>{activeUser.edad}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>Teléfono</p>
+                    <p className='textmuted mb-2'>{activeUser.telefono}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='box2 mt-3'>
+                <div className='d-flex mt-2'>
+                  <span className='fas fa-bookmark ms-0 mt-1'></span>
+                  <div className='w-100 ps-2 '>
+                    <p className=''>Pais</p>
+                    <p className='textmuted mb-2'>{activeUser.pais}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>Ciudad </p>
+                    <p className='textmuted mb-2'>{activeUser.ciudad}</p>
+                  </div>
+                  <div className='w-100 ps-2'>
+                    <p className=''>Codigo Postal</p>
+                    <p className='textmuted mb-2'>{activeUser.cpostal}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  )
 }
+
