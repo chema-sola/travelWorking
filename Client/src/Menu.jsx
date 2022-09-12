61dfbcd146f0457ec723d0e9e978e3e1602f9933
chema_Sola10 <chemitata10@gmail.com>
2022-09-12 12:26:05 +0200

    cambios

--- Client/src/Menu.jsx
+++ Client/src/Menu.jsx
@@ -14,32 +14,45 @@ export const Menu = () => {
           <Link to='/' className='navbar-brand nav-item nav-link '>
             <img className='hero__logoimg' style={{ width: '100px', height: '70px' }} src={Logo} />
           </Link>
-
-          <Link to='/ofertas' className='nav-item nav-link '>
-            {'Ofertas'}
-          </Link>
-
-          <Link to='/ofertadetallada' className='nav-item nav-link '>
-            {'Ofertadetallada'}
-          </Link>
-
-          {user.id && (
-            <Link to={`/perfil/${user.id}`} className='nav-link'>
-              {'Mi perfil'}
+          <div className='nav-item'>
+            <Link to='/ofertas' className='nav-item nav-link '>
+              {'Ofertas'}
             </Link>
-          )}
+          </div>
 
-          <Link to={`/perfil/2`} className='nav-link'>
-            {'No es mi perfil'}
-          </Link>
+          <div className='nav-item'>
+            <Link to='/ofertadetallada' className='nav-item nav-link '>
+              {'Ofertadetallada'}
+            </Link>
+          </div>
+
+          <div className='nav-item'>
+            {user.id && (
+              <Link to={`/perfil/${user.id}`} className='nav-link'>
+                {'Mi perfil'}
+              </Link>
+            )}
+          </div>
+
+          <div className='nav-item'>
+            <Link to={`/perfil/2`} className='nav-link'>
+              {'No es mi perfil'}
+            </Link>
+          </div>
 
 
           <Nav className='justify-content-end' style={{ width: '100%' }}>
             <Navbar.Brand>
               {user.id ? (
-                <button type="button" class="btn btn-lg btn-primary" data-toggle="popover" title="Perfil" data-content={user.nombre}>Perfil</button>
-
-                
+                <button
+                  type='button'
+                  className='btn btn-lg btn-primary'
+                  data-toggle='popover'
+                  title='Perfil'
+                  data-content={user.nombre}
+                >
+                  Perfil
+                </button>
               ) : (
                 <Link to='/login' className='nav-link'>
                   {'Login'}
@@ -47,8 +60,6 @@ export const Menu = () => {
               )}
             </Navbar.Brand>
           </Nav>
-
-
         </Navbar.Collapse>
       </Container>
     </Navbar>
