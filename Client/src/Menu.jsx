8a25b9670fc8b6cc58c9858dc6dfe6f748e10bf6
[Thecrawly] <[jtrod1@gmail.com]>
2022-09-14 10:27:13 +0200

    fix: menu

--- Client/src/Menu.jsx
+++ Client/src/Menu.jsx
@@ -7,38 +7,21 @@ export const Menu = () => {
   const { user } = useSelector((state) => state.auth)
 
   return (
-    <Navbar expand='lg' style={{ backgroundColor: '#e3f2fd' }}>
-      <Container className='menu'>
-        <Navbar.Toggle className='menu__togle' aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Link to='/' className='navbar-brand nav-item nav-link '>
-            <img className='hero__logoimg' style={{ width: '100px', height: '70px' }} src={Logo} />
-          </Link>
-          <Link to='/ofertas' className='nav-item nav-link '>
-            {'Ofertas'}
-          </Link>
-
-          {user.id && (
-            <Link to={`/perfil/${user.id}`} className='nav-link'>
-              {'Mi perfil'}
-            </Link>
-          )}
-          <Link to={`/perfil/2`} className='nav-link'>
-            {'No es mi perfil'}
-          </Link>
-          <Nav className='justify-content-end' style={{ width: '100%' }}>
-            <Navbar.Brand>
-              {user.id ? (
-                <p>{user.nombre}</p>
-              ) : (
-                <Link to='/login' className='nav-link'>
-                  {'Login'}
-                </Link>
-              )}
-            </Navbar.Brand>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <nav>
+  <ul>
+    <li>
+      <a href="#">Home</a>
+    </li>
+    <li>
+      <a href="#">About</a>
+    </li>
+    <li>
+      <a href="#">Services</a>
+    </li>
+    <li>
+      <a href="#">Contact</a>
+    </li>
+  </ul>
+</nav>
   )
 }
--- Server/config/config.js
+++ Server/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
   username: 'root',
-  password: 'root',
+  password: '',
   database: 'travelworking',
   host: '127.0.0.1',
   dialect: 'mysql',
