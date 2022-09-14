80ef5a498d281d349d6ef7022fb8ae536980fa60
[Thecrawly] <[jtrod1@gmail.com]>
2022-09-14 10:59:12 +0200

    f

--- Client/src/Menu.jsx
+++ Client/src/Menu.jsx
@@ -8,20 +8,20 @@ export const Menu = () => {
 
   return (
     <nav>
-  <ul>
-    <li>
-      <a href="#">Home</a>
-    </li>
-    <li>
-      <a href="#">About</a>
-    </li>
-    <li>
-      <a href="#">Services</a>
-    </li>
-    <li>
-      <a href="#">Contact</a>
-    </li>
-  </ul>
-</nav>
+      <ul>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Services</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
