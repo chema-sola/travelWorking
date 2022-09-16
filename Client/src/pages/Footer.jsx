7293dadea2a58c19fde5c6dcc4a8d103410ae456
[Thecrawly] <[jtrod1@gmail.com]>
2022-09-16 09:21:21 +0200

    footer-redes sociales

--- Client/src/pages/Footer.jsx
+++ Client/src/pages/Footer.jsx
@@ -16,7 +16,6 @@ function Footer() {
 
           <p className='footer-company-name'>Travel Working © 2022</p>
           <span className='footer-company-name'>Todos los derechos reservados</span>
-
         </div>
         <div className='footer-center'>
           <div>
@@ -32,20 +31,22 @@ function Footer() {
             <p>
               <a href='mailto:support@company.com'>soporte@travelworking.com</a>
             </p>
+            <span className='fa fa-map-marker'>Redes sociales</span>
+            <div className='footer__iconos'>
+              <FaFacebook size={30} />
+              <FaTwitter size={30} />
+              <FaLinkedin size={30} />
+              <FaInstagram size={30} />
+            </div>
           </div>
         </div>
         <div className='footer-right'>
           <p className='footer-company-about'>
             <span>Quienes somos</span>
-            Somos un grupo de personas con ganas de ayudar a los que desean cumplir su sueño de viajar, pero no
-            disponen de los recursos necesarios para ello. Creemos en un mundo justo donde debe haber oportunidades para todas las personas.{' '}
+            Somos un grupo de personas con ganas de ayudar a los que desean cumplir su sueño de viajar, pero no disponen
+            de los recursos necesarios para ello. Creemos en un mundo justo donde debe haber oportunidades para todas
+            las personas.{' '}
           </p>
-          <div className='footer__iconos'>
-            <FaFacebook size={30} />
-            <FaTwitter size={30} />
-            <FaLinkedin size={30} />
-            <FaInstagram size={30} />
-          </div>
         </div>
       </footer>
     </div>
