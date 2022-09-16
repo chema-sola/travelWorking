06e52b57bd57710b96642d5f452791f4e05937a6
[Thecrawly] <[jtrod1@gmail.com]>
2022-09-16 09:16:18 +0200

    footer informacion

--- Client/src/Menu.jsx
+++ Client/src/Menu.jsx
@@ -1,63 +1,19 @@
 import { Link } from 'react-router-dom'
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { useSelector } from 'react-redux'
-import Logo from './assets/img/LogoTravel.png'
 import { useAuthStore } from './hooks/useAuthStore'
+import Logo from './assets/img/LogoTravel.png'
+import { FiLogIn } from 'react-icons/fi'
 
 export const Menu = () => {
   const { user } = useSelector((state) => state.auth)
   const { logOut } = useAuthStore()
   return (
     <nav>
-      {/* <Container className='menu'>
-        <Navbar.Toggle className='menu__togle' aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Link to='/' className='navbar-brand nav-item nav-link '>
-            <img className='hero__logoimg' style={{ width: '100px', height: '70px' }} src={Logo} />
-          </Link>
-          <Link to='/ofertas' className='nav-item nav-link '>
-            {'Ofertas'}
-          </Link>
-
-          {user.id && (
-            <Link to={`/perfil/${user.id}`} className='nav-link'>
-              {'Mi perfil'}
-            </Link>
-          )}
-          {user.id && (
-            <>
-              {user.isHost === 1 ? (
-                <Link to={`/private/ofertas/${user.id}`} className='nav-link'>
-                  {'Mis ofertas'}
-                </Link>
-              ) : (
-                <Link to={`/private/candidaturas/${user.id}`} className='nav-link'>
-                  {'Mis candidaturas'}
-                </Link>
-              )}
-            </>
-          )}
-
-          {user.id && <Button onClick={logOut}>{'Log out'}</Button>}
-          <Nav className='justify-content-end' style={{ width: '100%' }}>
-            <Navbar.Brand>
-              {user.id ? (
-                <p>{user.nombre}</p>
-              ) : (
-                <Link to='/auth/login' className='nav-link'>
-                  {'Login'}
-                </Link>
-              )}
-            </Navbar.Brand>
-          </Nav>
-        </Navbar.Collapse>
-      </Container> */}
       <ul>
-        <li>
-          <Link to='/' className='navbar-brand nav-item nav-link '>
-            <img className='hero__logoimg' style={{ width: '70px', height: '50px' }} src={Logo} />
-          </Link>
-        </li>
+        <Link to='/' className='navbar-brand nav-item nav-link '>
+          <img className='hero__logoimg' style={{ width: '85px', height: '75px' }} src={Logo} />
+        </Link>
         <li>
           <Link to='/ofertas' className='nav-item nav-link '>
             {'Ofertas'}
@@ -89,17 +45,24 @@ export const Menu = () => {
         )}
 
         {user.id ? (
-          <li>{user.nombre}</li>
+          <li></li>
         ) : (
           <li>
-            <Link to='/auth/login' className='nav-link'>
+            <Link to='/auth/login' className='menu__logout btn btn-primary'>
               {'Login'}
             </Link>
           </li>
         )}
         {user.id && (
           <li>
-            <Button onClick={logOut}>{'Log out'}</Button>
+            <Button className='menu__logout' onClick={logOut}>
+              {
+                <>
+                  <FiLogIn />
+                  <p style={{ marginBottom: 0 }}>Log out</p>
+                </>
+              }
+            </Button>
           </li>
         )}
       </ul>
--- Client/src/pages/Footer.jsx
+++ Client/src/pages/Footer.jsx
@@ -1,31 +1,80 @@
 import { Link } from 'react-router-dom'
+import logo from '../assets/img/LogoTravel.png'
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa'
 
 import './footer.css'
 
-const Footer = () => (
-  <div className='footer'>
-    <div className='footer__center'>
-      <div className='footer__box'>
-        <div className='footer__title'>TravelWorking</div>
-        <div className='footer__content'>
-          <Link to='/'><p>Principal</p></Link>
-          <Link to='/ofertas'><p>Ofertas</p></Link>
-          <Link to='/login'><p>Login</p></Link>
-          <Link to='/register'><p>Registro</p></Link>
-          
-        </div>
-      </div>
-      <div className='footer__box'>
-        <div className='footer__title'>¿Quienes Somos?</div>
-        <div className='footer__content'>
-          <p>Ismael Cobo</p>
-          <p>Jonotahn</p>
-          <p>Wilmer</p>
-          <p>Chema Solà</p>
-        </div>
-      </div>
-    </div>
-  </div>
-)
+function Footer() {
+  return (
+    <div>
+      <footer className='footer-distributed'>
+        <div className='footer-left'>
+          <span>
+            {' '}
+            <img src={logo} alt='logo' style={{ width: '170px' }} />{' '}
+          </span>
 
+          <p className='footer-company-name'>Travel Working © 2022</p>
+          <span className='footer-company-name'>Todos los derechos reservados</span>
+
+        </div>
+        <div className='footer-center'>
+          <div>
+            <i className='fa fa-map-marker' />
+            <p>Contacto</p>
+          </div>
+          <div>
+            <i className='fa fa-phone' />
+            <p>+34.976.543.788</p>
+          </div>
+          <div>
+            <i className='fa fa-envelope' />
+            <p>
+              <a href='mailto:support@company.com'>soporte@travelworking.com</a>
+            </p>
+          </div>
+        </div>
+        <div className='footer-right'>
+          <p className='footer-company-about'>
+            <span>Quienes somos</span>
+            Somos un grupo de personas con ganas de ayudar a los que desean cumplir su sueño de viajar, pero no
+            disponen de los recursos necesarios para ello. Creemos en un mundo justo donde debe haber oportunidades para todas las personas.{' '}
+          </p>
+          <div className='footer__iconos'>
+            <FaFacebook size={30} />
+            <FaTwitter size={30} />
+            <FaLinkedin size={30} />
+            <FaInstagram size={30} />
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
 export default Footer
+
+// const Footer = () => (
+//   <div className='footer'>
+//     <div className='footer__center'>
+//       <div className='footer__box'>
+//         <div className='footer__title'>TravelWorking</div>
+//         <div className='footer__content'>
+//           <Link to='/'><p>Principal</p></Link>
+//           <Link to='/ofertas'><p>Ofertas</p></Link>
+//           <Link to='/login'><p>Login</p></Link>
+//           <Link to='/register'><p>Registro</p></Link>
+
+//         </div>
+//       </div>
+//       <div className='footer__box'>
+//         <div className='footer__title'>¿Quienes Somos?</div>
+//         <div className='footer__content'>
+//           <p>Ismael Cobo</p>
+//           <p>Jonatan</p>
+//           <p>Wilmer</p>
+//           <p>Chema Solà</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// )
