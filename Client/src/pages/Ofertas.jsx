f9ddf83b47b3969139c6aaa8f28e92b95b4c7f0a
Ismael Cobo <midismael@gmail.com>
2022-09-17 11:11:50 +0200

    fix: component infinite loop

--- Client/src/pages/Ofertas.jsx
+++ Client/src/pages/Ofertas.jsx
@@ -3,20 +3,29 @@ import { Container, Form } from 'react-bootstrap'
 import { Cartas } from '../Cartas/Cartas'
 import { useForm } from '../hooks'
 
+const formData = { buscador: '' }
+
 export const Ofertas = () => {
-  const [formValues, onChange] = useForm({ buscador: '' })
+  // Cuidado con poner un objeto dentro de  un hook, ya que como siempre es diferente
+  // siempre se va a renderizar de nuevo el componente
+  // Solucion:
+  // crear el objeto en una variable fuera del componente
+  const [formValues, onChange] = useForm(formData)
+
   return (
     <Container>
-      <Form.Group className='mb-3'>
-        <Form.Label>Buscar por palabra clave:</Form.Label>
-        <Form.Control
-          type='text'
-          className='cartas__buscador'
-          name='buscador'
-          onChange={onChange}
-          value={formValues.buscador}
-        />
-      </Form.Group>
+      <Form>
+        <Form.Group className='mb-3'>
+          <Form.Label>Buscar por palabra clave:</Form.Label>
+          <Form.Control
+            type='text'
+            className='cartas__buscador'
+            name='buscador'
+            onChange={onChange}
+            value={formValues.buscador}
+          />
+        </Form.Group>
+      </Form>
       <Cartas buscador={formValues.buscador} />
     </Container>
   )
